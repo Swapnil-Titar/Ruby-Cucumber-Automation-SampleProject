e0734ee425a68b448576d43fbb83ee66d8690289
Swapnil-Titar <swapniltitar03@gmail.com>
2022-03-29 13:07:12 +0530

    Create handsonwork.rb

---
+++ handsonwork.rb
@@ -0,0 +1,21 @@
+# Welcome to Ruby!
+
+print 'Hi this is Ruby'
+var = 90
+puts var
+puts 3**2
+puts 3+2
+puts 4/5
+puts 4-1
+puts 4%1
+var1 = "Exampletest"
+#puts var.length
+puts var1.length
+puts var1.reverse
+puts var1.upcase
+puts var1.downcase
+puts abc
+puts this is new era
+=begin 
+puts multiline comments in ruby
+=end
